38aacf397d8f50f03f78468e74f19be2c15becec
omborzov <omborzov@domclick.ru>
2020-06-13 13:40:27 +0300

    fix rollback and deploy

--- .ci-cd/deploy.sh
+++ .ci-cd/deploy.sh
@@ -1,17 +1,19 @@
 # Пример вызова скрипта
 # nohup ./ci-cd/deploy.sh dev "123456:ABCDEsaw" "-1001018" "docker.helloworld.com" &
+# nohup sh .ci-cd/deploy.sh master "bot1227874132:AAHdTV7_1aKNdq0pC1jO0wXXWCEgKb-0zBk" "-1001333389227" "docker.inseo.pro"
 
 # 1. Устанавливаем из входных параметров переменные окружения
 export HW_ENV=$1              # Окружение (dev/prod)
 export HW_TG_BOT_TOKEN=$2     # Токен бота Telegram (пример - bot110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw)
 export HW_TG_CHAT_ID=$3       # ID чата/канала в Telegram, куда бот будет отправлять сообщения (пример -1001234567890)
 export HW_DOCKER_REGISTRY=$4  # Хост репозитория Docker
+HW_BRANCH=$([ "${HW_ENV?}" "==" "prod" ] && echo master || echo dev)
 
-# 2. Деплой
+# 3. Деплой
 sh ./.ci-cd/curl_tg.sh "#deploy_start"
 
-/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:previous"
-/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:new" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:latest" "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:previous"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:new" "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:latest"
 /usr/local/bin/docker-compose -p "hw_back_${HW_ENV}" up -d  --remove-orphans
 
 sh ./.ci-cd/curl_tg.sh "#deploy_success"
--- .ci-cd/rollback.sh
+++ .ci-cd/rollback.sh
@@ -6,13 +6,14 @@ export HW_ENV=$1
 export HW_TG_BOT_TOKEN=$2
 export HW_TG_CHAT_ID=$3
 export HW_DOCKER_REGISTRY=$4  # Хост репозитория Docker
+HW_BRANCH=$([ "${HW_ENV?}" "==" "dev" ] && echo master || echo dev)
 
 # 2. Откат деплоя на предыдущую версию
 sh ./.ci-cd/curl_tg.sh "#start_rollback"
 
-/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:temp"
-/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:previous" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest"
-/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:temp" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:previous"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:latest" "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:temp"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:previous" "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:latest"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:temp" "${HW_DOCKER_REGISTRY}/hw_back_${HW_BRANCH}:previous"
 /usr/local/bin/docker-compose up -d
 
 sh ./.ci-cd/curl_tg.sh "#end_rollback"
