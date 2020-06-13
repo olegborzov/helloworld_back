319bb0208282f35220e6e3440d124fc9154bcfea
omborzov <omborzov@domclick.ru>
2020-06-13 16:07:37 +0300

    fix ci-cd scripts comments

--- .ci-cd/deploy.sh
+++ .ci-cd/deploy.sh
@@ -7,6 +7,7 @@
 #   "110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw" \
 #   "-1001234567890"
 
+
 # 1. Устанавливаем из входных параметров переменные окружения
 export HW_ENV=$1
 HW_BRANCH=$([ "${HW_ENV?}" == 'prod' ] && echo master || echo dev)
@@ -15,6 +16,7 @@ HW_DOCKER_LOGIN=$3            # Логин для репозитория Docker
 HW_DOCKER_PASSWORD=$4         # Пароль для репозитория Docker
 HW_TG_BOT_TOKEN=$5            # Токен для бота Telegram
 HW_TG_CHAT_ID=$6              # ID чата/канала в Telegram для отправки уведомлений
+
 
 # 2. Деплой
 sh ./.ci-cd/curl_tg.sh "$HW_TG_BOT_TOKEN" "$HW_TG_CHAT_ID" "$HW_ENV" "#deploy_start"
