945f815c1a80e45194dca19f1e539b718c51ec6a
omborzov <omborzov@domclick.ru>
2020-06-13 13:17:02 +0300

    fix deploy and rollback

--- .ci-cd/deploy.sh
+++ .ci-cd/deploy.sh
@@ -1,17 +1,18 @@
 # Пример вызова скрипта
-# nohup ./ci-cd/deploy.sh dev "123456:ABCDEsaw" "-1001018" &
+# nohup ./ci-cd/deploy.sh dev "123456:ABCDEsaw" "-1001018" "docker.helloworld.com" &
 
 # 1. Устанавливаем из входных параметров переменные окружения
-export HW_ENV=$1            # Окружение (dev/prod)
-export HW_TG_BOT_TOKEN=$2   # Токен бота Telegram (пример - bot110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw)
-export HW_TG_CHAT_ID=$3     # ID чата/канала в Telegram, куда бот будет отправлять сообщения (пример -1001234567890)
+export HW_ENV=$1              # Окружение (dev/prod)
+export HW_TG_BOT_TOKEN=$2     # Токен бота Telegram (пример - bot110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw)
+export HW_TG_CHAT_ID=$3       # ID чата/канала в Telegram, куда бот будет отправлять сообщения (пример -1001234567890)
+export HW_DOCKER_REGISTRY=$4  # Хост репозитория Docker
 
 # 2. Деплой
 sh ./.ci-cd/curl_tg.sh "#deploy_start"
 
-docker tag "hw_back_${HW_ENV}:latest" "hw_back_${HW_ENV}:previous"
-docker tag "hw_back_${HW_ENV}:new" "hw_back_${HW_ENV}:latest"
-docker-compose -p "hw_back_${HW_ENV}" up -d  --remove-orphans
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:previous"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:new" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest"
+/usr/local/bin/docker-compose -p "hw_back_${HW_ENV}" up -d  --remove-orphans
 
 sh ./.ci-cd/curl_tg.sh "#deploy_success"
 
--- .ci-cd/rollback.sh
+++ .ci-cd/rollback.sh
@@ -1,17 +1,18 @@
 # Пример вызова скрипта
-# nohup ./ci-cd/rollback.sh dev "123456:ABCDEsaw" "-1001018" &
+# nohup ./ci-cd/rollback.sh dev "123456:ABCDEsaw" "-1001018" "docker.helloworld.com" &
 
 # 1. Устанавливаем из входных параметров переменные окружения
 export HW_ENV=$1
 export HW_TG_BOT_TOKEN=$2
 export HW_TG_CHAT_ID=$3
+export HW_DOCKER_REGISTRY=$4  # Хост репозитория Docker
 
 # 2. Откат деплоя на предыдущую версию
 sh ./.ci-cd/curl_tg.sh "#start_rollback"
 
-docker tag "hw_back_${HW_ENV}:latest" "hw_back_${HW_ENV}:temp"
-docker tag "hw_back_${HW_ENV}:previous" "hw_back_${HW_ENV}:latest"
-docker tag "hw_back_${HW_ENV}:temp" "hw_back_${HW_ENV}:previous"
-docker-compose up -d
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:temp"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:previous" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:latest"
+/usr/bin/docker tag "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:temp" "${HW_DOCKER_REGISTRY}/hw_back_${HW_ENV}:previous"
+/usr/local/bin/docker-compose up -d
 
 sh ./.ci-cd/curl_tg.sh "#end_rollback"
