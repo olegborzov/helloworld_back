cc61107975cf95c9918585c6830acc5320a29ed7
Oleg Borzov <mail@olegborzov.ru>
2020-06-16 12:05:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ class Config:
     )
 
     # EMAIL Reports Credentials
-    SMTP_LOGIN = env.str("SMTP_LOGIN", default="rambler.seo.reports@gmail.com")
-    SMTP_PASS = env.str("SMTP_PASS", default="a$3sdj1kh")
+    SMTP_LOGIN = env.str("SMTP_LOGIN", default=")
+    SMTP_PASS = env.str("SMTP_PASS", default="")
     SMTP_SERVER = env.str("SMTP_SERVER", default="smtp.gmail.com")
     SMTP_PORT = env.int("SMTP_PORT", default=587)
